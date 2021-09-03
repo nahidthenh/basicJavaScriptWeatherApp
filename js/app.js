5e0e2aaf19c1329434eb0277a6b733bd7e3adabe
nahidthenh <nhgroup18@gmail.com>
2021-09-03 10:57:04 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+
+
+// MyCode 
+
+const API_KEY = `f8c19cf71f59a4f49f5f59aff4a3ce85`;
+
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data))
+
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = (temperature) => {
+    setInnerText('city', temperature.name)
+    setInnerText('temperature', temperature.main.temp)
+    setInnerText('condition', temperature.weather[0].main)
+    // setInnerText('weather-icon',temperature.weather[0].icon)
+
+    // Set weather icon 
+
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url)
+}
